03c48be5d08affe81b59e2c335a2212cb360369c
Javier <jarrindaulivi4@gmail.com>
2018-12-29 13:32:18 -0500

    commit

--- native/src/App.js
+++ native/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  thisIsTheJumpToFunction = (e) =>{
+    e.preventDefault()
+  }
+
+  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={thisIsTheJumpToFunction()}>
+          <img src="https://bit.ly/2ifCX2U" className="App-logo" alt="logo" />
+
+        </button>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
